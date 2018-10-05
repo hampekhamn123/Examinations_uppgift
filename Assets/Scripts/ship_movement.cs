965d58b2fa300b4fe902a456112f5c53357384fc
Hampus <hampus.andersson@elev.ga.lbs.se>
2018-10-05 16:05:48 +0200

    Har gjort klart alla E och håller på med C

---
+++ Assets/Scripts/ship_movement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ship_movement : MonoBehaviour
+{
+    public SpriteRenderer rend;
+    private float boatSpeed = 5;
+    public Color boatColor;
+    private int rotationSpeed = 3;
+    private float timer = 0;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //skeppet åker uppåt i y axeln i hastigheten 5 oavset FPS
+        transform.Translate(0, boatSpeed * Time.deltaTime, 0);
+
+        //när S trycks ner vill den halva hastigheten åt andra hållet vilket reducerar hastigheten
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0f, (-boatSpeed / 2) * Time.deltaTime, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0f, 0f, rotationSpeed);
+            rend.color = Color.green;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0f, 0f, -rotationSpeed);
+            rend.color = Color.blue;
+        }
+
+        timer += Time.deltaTime;
+        
+    }
+}
